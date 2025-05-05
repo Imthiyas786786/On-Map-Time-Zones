b5d7f5255667398dc62921e2c0ab218c5d9639bf
Imthiyas786786 <shaik.mohamed.s92@kalvium.community>
2025-05-05 10:55:54 +0530

    redirect.js

---
+++ redirect.js
@@ -0,0 +1,4 @@
+// This script waits for 3 seconds and then redirects the user to the homepage
+setTimeout(() => {
+    window.location.href = "home.html"; // Replace 'homepage.html' with your homepage URL
+}, 3000);
